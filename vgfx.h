b4d7fae64124ea2aff38bf0241351cb787e11303
JT <60622543+HarryFishnuts@users.noreply.github.com>
2022-10-02 13:28:53 -0400

    Since VCORE has been updated, I am rewriting everything.

--- vgfx.h
+++ vgfx.h
@@ -6,11 +6,4 @@
 #ifndef _VGFX_INCLUDE_
 #define _VGFX_INCLUDE_
 
-/* ========== INCLUDES							==========	*/
-#include "vcore.h"				/* all core functions and defs	*/
-#include "vgfxdefs.h"			/* all gfx related defs			*/
-#include "vgfxcore.h"			/* core gfx functionality		*/
-#include "vgfxshaders.h"		/* shader creation				*/
-#include "vgfxobjects.h"		/* gfx object manipulation		*/
-
-#endif 
+#endif
